e6fde50678d3db573643d721db98570d7a80cd28
Kenneth Fan <kf6999@rit.edu>
2022-08-01 08:52:15 -0700

    Fixed date format

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 	"weight.kenfan.org/internal/models"
 )
 
@@ -10,6 +11,14 @@ type templateData struct {
 	Weight      *models.Weight
 	Weights     []*models.Weight
 	CurrentYear int
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("Jan 02 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,7 +33,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		name := filepath.Base(page)
 
 		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
