807d5da95b715f57da55807de73610fea2bec91e
Kenneth Fan <kf6999@rit.edu>
2022-08-01 09:08:40 -0700

    Set security headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 // The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/weight/view", app.weightView)
 	mux.HandleFunc("/weight/create", app.weightCreate)
 
-	return mux
+	return secureHeaders(mux)
 }
