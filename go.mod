b0ff3eca2c616b8a675d4d2bb02f9ce4dc3d0685
Kenneth Fan <kf6999@rit.edu>
2022-08-03 10:46:42 -0700

    Added user sign up

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/justinas/alice v1.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.14 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 )
--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
@@ -17,6 +18,16 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+    VALUES(?, ?, ?, CURRENT_TIMESTAMP)`
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		return ErrDuplicateEmail
+	}
 	return nil
 }
 
